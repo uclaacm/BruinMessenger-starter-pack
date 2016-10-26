4b7fc308e50838375a350bf269f33cc7d8772bdf
Ky-Cuong Huynh <kycodehuynh@gmail.com>
2016-10-26 16:41:40 -0700

    Mark TODOs for Hack School teams

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
+// Pull in everything we need to use, including Express
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-// set the view engine to handlebars
+// Set the view engine to handlebars
 app.use(express.static('public'))
 app.set('view engine', 'hbs')
 
+// TODO: Serve up the login page
 app.get('/', (req, res, next) => {
-  res.render('login')
+    // TODO
 })
 
+// TODO: Serve up the rooms page
 app.get('/rooms', (req, res, next) => {
-  res.render('rooms')
+    // TODO
 })
 
-// 404 for all unregistered routes
+// TODO: 404 for all unregistered routes
 app.use((req, res, next) => {
-  res.status(404).render('404')
 })
 
 app.listen(port, () => {
