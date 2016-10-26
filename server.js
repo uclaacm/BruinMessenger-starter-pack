b5bb73046589658fe543e60a9ee282bd62a4a121
mitrikyle <mitrikyle@yahoo.com>
2016-10-25 21:08:16 -0700

    Deletd my life's work

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const bruinMessenger = require('./app')
-const passport = require('passport')
 
 // set the view engine to handlebars
 app.use(express.static('public'))
 app.set('view engine', 'hbs')
 
-// include session middleware
-app.use(bruinMessenger.session)
-// connect passport middleware
-app.use(passport.initialize())
-// connect express-session to passport
-app.use(passport.session())
+app.get('/', (req, res, next) => {
+  res.render('login')
+})
 
-// Register our bruinMessenger routes
-app.use('/', bruinMessenger.router)
+app.get('/rooms', (req, res, next) => {
+  res.render('rooms')
+})
 
 // 404 for all unregistered routes
 app.use((req, res, next) => {
   res.status(404).render('404')
 })
 
-bruinMessenger.ioServer(app).listen(port, () => {
+app.listen(port, () => {
   console.log('BruinMessenger running on port: ', port)
 })
